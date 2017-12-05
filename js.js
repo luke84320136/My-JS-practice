81a9d2d32d354558f9d58a639f3c241d03a3362d
Luke Lan <luke84320136@gmail.com>
2017-12-05 17:33:05 +0800

    Add files via upload

--- js.js
+++ js.js
@@ -68,3 +68,204 @@
 
 // let user = new User("John", new Date(2000,0,1));
 // user.sayHi(); // John
+
+// function HEY() {
+//   var section = "HELLO"
+//   console.log(section)
+// }
+// HEY()
+// console.log(section)
+
+// if (true) {
+//   var test = true
+// }
+
+// console.log(test)
+
+// for(var i = 10 ; i >4; i--) {
+
+// }
+// console.log(i)
+
+// function Yo () {
+//   if (false) {
+//     var text = 'Yo'
+//   }
+//   console.log(text)
+// }
+// text()
+// console.log(text)
+
+// function Help() {
+//   console.log(text)
+
+//   var text = "hello"
+// }
+// Help()
+
+// JS.INFO 6.5 EX
+// var section = "HEY"
+// function test () {
+//   console.log(section)
+// }
+
+// console.log(window.section)
+// console.log(window.test)
+
+// let user= 'test'
+// console.log(user)
+// console.log(window.user) //undeifind
+// console.log('user' in window) //false
+
+// var user = 'Global'
+// function fn() {
+//   var user = 'Local'
+//   alert(window.user) // Global
+// }
+// fn()
+
+// console.log( this === window )
+
+// let testFun = () => {
+//   console.log(this)
+// }
+
+// testFun()
+
+// JS.INFO 6.6 EX
+// let HiFun = () => {
+//   console.log('Hi')
+
+//   HiFun.counter ++
+// }
+// HiFun.counter = 0
+// HiFun()
+// HiFun()
+//  console.log(`Call ${HiFun.counter} times`)
+
+// let HiFun = () => {
+//   console.log('hi')
+// }
+// console.log(HiFun.name)
+
+// let user = {
+//   HiFun() {
+
+//   },
+//   ByeFun: function() {
+
+//   }
+// }
+// console.log(user.HiFun.name)
+// console.log(user.ByeFun.name)
+
+// function makeCount() {
+//   function counter () {
+//     return counter.count ++
+//   }
+//   counter.count = 0
+//   return counter
+// }
+// let counter = makeCount()
+// console.log( counter())
+// console.log( counter())
+
+// JS.INFO 6.7 'new Function'
+
+// function getFun () {
+//   let value = 'test'
+//   let func = new Function('console.log(value)')
+//   return func
+// }
+// getFun()() //value is not defined
+// function getFun () {
+//   let value = 'test'
+//   let func = function(){
+//     'console.log(value)'
+//   }
+//   return func
+// }
+// getFun()() //
+
+// JS.INFO 6.8 'setTimeout & setIterval'
+// function test () {
+//   console.log('hello')
+// }
+// setTimeout(test, 1000)
+
+// function fFuN(section, who) {
+//  console.log(section + ',' + who)
+// }
+// setTimeout(fFuN, 1000, 'Hey', 'world')
+
+// test defaultOptions
+// let dOpt = {
+//   img: 4,
+//   index: 4,
+//   padding: 5
+// }
+
+// const slide = () => {
+//   let index = dOpt.index
+//   console.log(index)
+// }
+
+// const gPage = (i) => {
+//   return Math.floor(dOpt.index / dOpt.img)
+// }
+
+// console.log(gPage())
+
+// JS.INFO 7.6 'Methods for prototypes'
+
+// let animal = {
+//   eat: true
+// }
+
+// let pig = Object.create(animal, {
+//   drink : {
+//     value: 'Yap'
+//   }
+// })
+// console.log(pig.drink)
+// 'Very plain' object EX1
+// let obj = Object.create(null)
+// console.log(obj)
+// 'Very plain' object EX2 & Object.keys(obj)
+// let chDic = Object.create(null)
+// chDic.hello = '你好'
+// chDic.bye  = '88'
+
+// console.log(Object.entries(chDic))
+// All properties
+// let animal = {
+//   eats: 'eat somthing!'
+// }
+
+// let doggy = {
+//   talk: 'woof',
+//   __proto__: animal
+// }
+// console.log(Object.keys(doggy))
+// for(let prop in doggy) console.log(prop)
+// JS.INFO 7.7 'Class patterns'
+// let User= (name) => {
+//   return {
+//     sayHi: ()=> {
+//       console.log(name)
+//     }
+//   }
+// }
+// let user = User('Hey')
+// user.sayHi()
+
+// var greeting = function () {
+
+// }
+
+// closeTerminal(greeting)
+// function closeTerminal () {
+//   message()
+// }
+
+console.log('a' + 'b')
